35cb45947ccf4bd67925f0ca6110928fd2ab5383
aoi0038 <aoi0038@gmail.com>
2022-02-24 19:21:57 +0900

    ディレクトリ追加に伴うファイル移動および修正

---
+++ src/public/user/signin_complete.php
@@ -0,0 +1,25 @@
+<?php
+require_once(__DIR__ . '/../../app/Infrastructure/Dao/UserDao.php');
+require_once __DIR__ . '/../../app/Infrastructure/Redirect/redirect.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+session_start();
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+
+if (empty($email) || empty($password)) {
+    $_SESSION['errors'][] = "パスワードとメールアドレスを入力してください";
+    redirect("./signin.php");
+}
+
+$userDao = new UserDao();
+$member = $userDao->findByEmail($email);
+
+if (!password_verify($password, $member["password"])) {
+    $_SESSION['errors'][] = "メールアドレスまたは<br />パスワードが違います";
+    redirect("./signin.php");
+}
+
+$_SESSION['formInputs']['userId'] = $member['id'];
+$_SESSION['formInputs']['name'] = $member['name'];
+// var_dump($_SESSION);die;
+redirect("../index.php");
